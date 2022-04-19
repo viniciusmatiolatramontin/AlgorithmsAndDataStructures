e502dfc671c399b4f4dc11ad8bb76577eab9673e
Vinícius Matiola Tramontin <vtramontin@furb.br>
2022-04-19 10:22:36 -0300

    Created Tree data structure

---
+++ Estruturas de dados/src/linear/lists/Lista.java
@@ -0,0 +1,24 @@
+
+public interface Lista<T> {
+
+	void inserir(T valor);
+
+	int buscar(T valor);
+
+	void retirar(T valor);
+
+	String exibir();
+
+	boolean estaVazia();
+
+	void concatenar(Lista<T> outra);
+
+	Lista<T> dividir();
+
+	Lista<T> copiar();
+
+	int getTamanho();
+
+	T pegar(int pos);
+
+}
